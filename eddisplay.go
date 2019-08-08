7200cc320b230ddbb145d28b6a75665b3d7c185a
Peter Birk Pakkenberg <peter.birk.nielsen+github@gmail.com>
2019-08-08 22:15:13 +0100

    Removed all DirectOutput garbage.

--- eddisplay.go
+++ eddisplay.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/peterbn/edx52display/directoutput"
 )
 
 func main() {
-	directoutput.Initialize("Hello, World")
-	defer directoutput.Deinitialize()
-	fmt.Println("Initialized program")
+
 	fmt.Scanln()
 }
