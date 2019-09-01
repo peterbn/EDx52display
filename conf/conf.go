f735e437eeed94304ecda5762fb5c954d45a9dcd
Peter Birk Pakkenberg <peter.birk.nielsen+github@gmail.com>
2019-09-01 14:35:55 +0100

    Revert "Added config for EDSM commande name and API key."
    This reverts commit 37b86d94cc8717639fc6d6a2136a976ff8cbdad5.

--- conf/conf.go
+++ conf/conf.go
@@ -13,10 +13,8 @@ import (
 
 // Conf is the app config
 type Conf struct {
-	JournalsFolder    string
-	RefreshRateMS     int
-	EDSMCommanderName string
-	EDSMAPIKey        string
+	JournalsFolder string
+	RefreshRateMS  int
 }
 
 // LoadConf loads the config from the yaml file
